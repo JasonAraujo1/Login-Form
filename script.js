24830f38afe5d9e270148f591830832e2d3db6dc
Jason A. <you@example.com>
2023-10-19 23:01:25 -0300

    js input color focus

---
+++ script.js
@@ -0,0 +1,23 @@
+//INPUT ICONS COLOR 
+
+const inputEmail = document.getElementById('E-mail')
+const iconEmail = document.getElementById('icon-email')
+
+const senhaInput = document.getElementById('senha')
+const senhaIcon = document.getElementById('icon-senha')
+
+inputEmail.addEventListener('focus',() => {
+    iconEmail.style.color = '#FFC632';
+})
+
+inputEmail.addEventListener('blur',() => {
+    iconEmail.style.color = '#AFB6C2';
+})
+
+senhaInput.addEventListener('focus', ()  => {
+    senhaIcon.style.color = '#FFC632';
+}) 
+
+senhaInput.addEventListener('blur', ()  => {
+    senhaIcon.style.color = '#AFB6C2';
+}) 
