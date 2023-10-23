50871b06c557323ad59f367cf3cd9b320e7f69b0
Jason A. <you@example.com>
2023-10-23 17:02:30 -0300

    eye icon finish

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const iconEmail = document.getElementById('icon-email')
 
 const senhaInput = document.getElementById('senha')
 const senhaIcon = document.getElementById('icon-senha')
+const eyeIcon = document.getElementById('eye-icon')
+
+
 
 inputEmail.addEventListener('focus',() => {
     iconEmail.style.color = '#FFC632';
@@ -16,11 +19,14 @@ inputEmail.addEventListener('blur',() => {
 
 senhaInput.addEventListener('focus', ()  => {
     senhaIcon.style.color = '#FFC632';
+    eyeIcon.style.color = '#FFC632';
 }) 
 
 senhaInput.addEventListener('blur', ()  => {
     senhaIcon.style.color = '#AFB6C2';
+    eyeIcon.style.color = '#AFB6C2';
 }) 
+
 
 
 
@@ -31,6 +37,13 @@ function mostrarSenha(){
 
     if(inputPass.type === 'password'){//aqui diz que : se o tipo da minha variável 'input pass' for exatamente igual a password eu quero que mude algo...
         inputPass.setAttribute('type','text')//aqui vai mudar tipo do atributo do input para texto
+        btnShowPass.classList.replace('bi-eye-slash', 'bi-eye')
+    }
+    else{
+        inputPass.setAttribute('type', 'password')
         btnShowPass.classList.replace('bi-eye', 'bi-eye-slash')
     }
+
+
 }
+
